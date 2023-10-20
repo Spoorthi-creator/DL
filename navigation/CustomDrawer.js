4054e67dd1acf4983011d941d15147a382592ef0
Spoorthi-creator <peetu88@gmail.com>
2023-10-20 20:23:51 +0530

    Custom drawer

--- navigation/CustomDrawer.js
+++ navigation/CustomDrawer.js
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
@@ -10,28 +11,105 @@ import {
   DrawerContentScrollView,
   DrawerItemList,
 } from '@react-navigation/drawer';
-
+import firebase from 'firebase';
+import db from '../screens/config'
+import * as ImagePicker from 'expo-image-picker';
 const CustomDrawer = props => {
+  const [name,setName]=useState('');
+  const email=firebase.auth().currentUser.email;
+  const[image,setImage]=useState('https://dummyimage.com/80x80/000/fff.png');
+  const getUserDetails27 = () => {
+    db.collection("users")
+      .where("email", "==", email)
+      .onSnapshot((snapshot) => {
+        snapshot.docs.map((doc) => {
+         setName(doc.data().name)
+        })
+    });}
+    useEffect(() => {
+
+    getUserDetails27();
+    fetchImage();
+ 
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log('Result', result.assets[0].uri);
+
+    if (!result.cancelled) {
+      setImage(result.assets[0].uri ); 
+      uploadImage(result.assets[0].uri);
+    }
+  };
+
+  const uploadImage = async (imageUri) => {
+    try {
+      const blob = await new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+          resolve(xhr.response);
+        };
+        xhr.onerror = function () {
+          reject(new TypeError('Network request failed'));
+        };
+        xhr.responseType = 'blob';
+        xhr.open('GET', imageUri, true);
+        xhr.send(null);
+      });
+
+      await firebase
+        .storage()
+        .ref()
+        .child('usersP/' + firebase.auth().currentUser.email)
+        .put(blob);
+
+      fetchImage();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchImage = async () => {
+    try {
+      const url = await firebase
+        .storage()
+        .ref()
+        .child('usersP/' + firebase.auth().currentUser.email)
+        .getDownloadURL();
+       setImage( url) ;
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <View style={{flex: 1}}>
       <DrawerContentScrollView
         {...props}
         contentContainerStyle={{backgroundColor: '#1c0f24'}}>
-        <ImageBackground
-          source={require('../assets/drawer.png')}
-          style={{padding: 20}}>
-     
+       
+       <TouchableOpacity onPress={pickImage} style={{ alignItems: 'center' }}>
+          <Image
+          source={{ uri: image }}
+          style={{ width:50, height:50, alignSelf:'center', borderRadius:80, borderWidth:2,borderColor:'white'}}/>
+        </TouchableOpacity>   
           <Text
             style={{
               color: '#fff',
               fontSize: 18,
-             
+             marginLeft:20,
               marginBottom: 5,
             }}>
-          Digital Lib
+      {name}
           </Text>
        
-        </ImageBackground>
+      
         <View style={{flex: 1, backgroundColor: '#fff', paddingTop: 10}}>
           <DrawerItemList {...props} />  
         </View>
--- screens/config.js
+++ screens/config.js
@@ -1,13 +1,12 @@
 import firebase from 'firebase';
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDa5YuAH4Czc2LR3cHSrp47ZAAHSFQiSAo",
-  authDomain: "digilibrary-fa3a4.firebaseapp.com",
-  projectId: "digilibrary-fa3a4",
-  storageBucket: "digilibrary-fa3a4.appspot.com",
-  messagingSenderId: "387231899601",
-  appId: "1:387231899601:web:b5b9a431bf7e068b634af0",
-  measurementId: "G-7X0GE53DFH"
+  apiKey: "AIzaSyCJ14_u7XmvsKKjRM059kiPa0ZHdn9L7u4",
+  authDomain: "digilib-f0c97.firebaseapp.com",
+  projectId: "digilib-f0c97",
+  storageBucket: "digilib-f0c97.appspot.com",
+  messagingSenderId: "633140166428",
+  appId: "1:633140166428:web:1e91cde16aa0e1766d44d9"
 };
 
 if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
